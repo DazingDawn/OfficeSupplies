50974bfce56473d087502bcd799f4dc3a97f0adc
Soren Fey <sfey25@students.polytechnic.org>
2024-02-01 10:05:49 -0800

    more to deskchair

--- DeskChair.java
+++ DeskChair.java
@@ -4,17 +4,22 @@ public class DeskChair
     public double rotation = 0.0;
     public double height = 3.0;
     public double newRotation = 0.0;
-    public double rotate(rotChange)
+    public double rotate()
     {
-        newRotation = rotation + rotChange;
+        newRotation = rotation + 5;
         if (newRotation <= 360.0 || newRotation >= 0.0)
         {
             rotation = newRotation;
         }
         else if (newRotation > 360.0)
         {
-            
+            newRotation = newRotation - 360.0;
+        }
+        else if (newRotation < 0.0)
+        {
+            newRotation = newRotation + 360.0;
         }
     }
+    System.out.println("the old rotation was 0.0. the new rotation is " + rotation + " and the expected value was 5.0.");
 
 }
