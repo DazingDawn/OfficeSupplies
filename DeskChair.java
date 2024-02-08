0a1323b837258d8030301d44084583a34ff1515b
Soren Fey <sfey25@students.polytechnic.org>
2024-02-07 18:00:52 -0800

    Tester for counter and deskchair complete, training wheels removed for both programs.

--- DeskChair.java
+++ DeskChair.java
@@ -5,101 +5,57 @@ public class DeskChair
     public double height = 3.0;
     public double x = 0.0;
     public double y = 0.0;
-    public double newRotation = 0.0;
-    public double newAngle = 0.0;
-    public double newHeight = 0.0;
-    public double newX = 0.0;
-    public double newY = 0.0;
 
-    public void rotate() 
+    public void rotate(double newRotation) 
     {
-        newRotation = rotation + -10.0;
-        if (newRotation <= 360.0 && newRotation >= 0.0) 
+        rotation += newRotation;
+        if (rotation >= 360.0) 
         {
-            rotation = newRotation;
+            rotation -= 360.0;
         } 
-        else if (newRotation > 360.0) 
+        else if (rotation < 0.0) 
         {
-            rotation = newRotation - 360.0;
-        } 
-        else if (newRotation < 0.0) 
-        {
-            rotation = newRotation + 360.0;
+            rotation += 360.0;
         }
     }
-    public void changeAngle()
-    {
-        newAngle = angle + -10.0;
-        if (newAngle <= 90.0 && newAngle >= 0.0) 
-        {
-            angle = newAngle;
-        } 
-        else if (newAngle > 90.0) 
-        {
-            angle = newAngle - 90.0;
-        } 
-        else if (newAngle < 0.0) 
-        {
-            angle = newAngle + 90.0;
-        }
-    }
-    public void changeHeight()
-    {
-        newHeight = height + -10.0;
-        if (newHeight <= 8.0 && newHeight >= 0.0) 
-        {
-            height = newHeight;
-        } 
-        else if (newHeight > 8.0) 
-        {
-            newHeight = 8.0;
-        } 
-        else if (newHeight < 0.0) 
-        {
-            newHeight = 0.0;
-        }
-    }
-    public void move()
-    {
-        newX = x + -10.0;
-        newY = y + 35;
-        if (newX <= 50.0 && newX >= -50.0) 
-        {
-            x = newX;
-        } 
-        else if (newX > 50.0) 
-        {
-            newX = 50.0;
-        } 
-        else if (newX < -50.0) 
-        {
-            newX = -50.0;
-        }
-        if (newY <= 50.0 && newY >= -50.0) 
-        {
-            y = newY;
-        } 
-        else if (newY > 50.0) 
-        {
-            newY = 50.0;
-        } 
-        else if (newY < -50.0) 
-        {
-            newY = -50.0;
-        }
 
-    }
-    public static void main(String[] args) 
+    public void changeAngle() 
     {
-        DeskChair deskChair = new DeskChair(); // Create an instance
-        deskChair.rotate(); // Call the rotate method
-        System.out.println("the old rotation was 0.0. the new rotation is " + deskChair.rotation + " and the expected value was 10.0.");
-        deskChair.changeAngle();
-        System.out.println("the old angle was 45.0. the new angle is " + deskChair.angle + " and the expected value was 35.0.");
-        deskChair.changeHeight();
-        System.out.println("the old height was 3.0. the new height is " + deskChair.height + " and the expected value was 0.0.");
-        deskChair.move();
-        System.out.println("the old X was 0.0. the new X is " + deskChair.x + " and the expected value was -10.0.");
-        System.out.println("the old Y was 0.0. the new Y is " + deskChair.y + " and the expected value was 35.0.");
+        angle -= 10.0;
+        if (angle < 0.0) 
+        {
+            angle += 90.0;
+        }
+    }
+
+    public void changeHeight() 
+    {
+        height -= 10.0;
+        if (height < 0.0) 
+        {
+            height = 0.0;
+        }
+    }
+
+    public void move() 
+    {
+        x -= 10.0;
+        y += 35.0;
+        if (x < -50.0) 
+        {
+            x = -50.0;
+        } 
+        else if (x > 50.0) 
+        {
+            x = 50.0;
+        }
+        if (y < -50.0) 
+        {
+            y = -50.0;
+        } 
+        else if (y > 50.0) 
+        {
+            y = 50.0;
+        }
     }
 }
---
+++ OfficeSuppliesTester.java
@@ -0,0 +1,35 @@
+public class OfficeSuppliesTester 
+{
+    public static void main(String[] args)
+    {
+        int testsPassed = 0;
+        System.out.println("Testing Counter.java!");
+        Counter clicker = new Counter();
+        testsPassed++;
+        System.out.println("New counter created!");
+        System.out.println("Trying to add to count...");
+        clicker.click();
+        testsPassed++;
+        System.out.println("Done!");
+        System.out.println("Trying to check count...");
+        System.out.println("Current count is " + clicker.getClicks());
+        testsPassed++;
+        System.out.println("Trying to reset count...");
+        clicker.reset();
+        testsPassed++;
+        System.out.println("Counter has passed " + testsPassed + "/4 tests!");
+        testsPassed = 0;
+        System.out.println("Testing DeskChair.java!");
+        DeskChair deskChair = new DeskChair();
+        deskChair.rotate(35.0);
+        System.out.println("The old rotation was 0.0. The new rotation is " + deskChair.rotation + " and the expected value was 35.0.");
+        deskChair.changeAngle();
+        System.out.println("The old angle was 45.0. The new angle is " + deskChair.angle + " and the expected value was 35.0.");
+        deskChair.changeHeight();
+        System.out.println("The old height was 3.0. The new height is " + deskChair.height + " and the expected value was 0.0.");
+        deskChair.move();
+        System.out.println("The old X was 0.0. The new X is " + deskChair.x + " and the expected value was -10.0.");
+        System.out.println("The old Y was 0.0. The new Y is " + deskChair.y + " and the expected value was 35.0.");
+
+    }
+}
